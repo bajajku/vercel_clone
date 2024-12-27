0307f428331db85ca6e9546565636789fd37e7f1
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2024-12-27 21:54:28 +0000

    boilerplate for app

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // middleware to parse incoming requests with JSON payloads
 
+// initializes a single endpoint that accepts POST requests
+app.post("/deploy", (req, res) => {
+
+    // github url
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+
+    res.json({ message: "Deploying..." });
+})
 app.listen(3000);
