d47d283ae1b9dfae014a3dbb649e6a4e581aa30c
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2024-12-27 22:16:06 +0000

    Implement deploy endpoint with random ID generation and repository cloning

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import simpleGit from 'simple-git';
+import { generateRandomId } from './utils';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // middleware to parse incoming requests with JSON payloads
 
 // initializes a single endpoint that accepts POST requests
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
 
     // github url
     const repoUrl = req.body.repoUrl;
-    console.log(repoUrl);
-
+    const id = generateRandomId();
+    await simpleGit().clone(repoUrl, `./output/${id}`);
     res.json({ message: "Deploying..." });
 })
 app.listen(3000);
